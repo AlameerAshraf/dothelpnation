8ed493489e752aecc24fe9184649a79a0ce199f4
Alameer Ashraf <alamiir.ashraf@gmail.com>
2018-08-18 20:37:54 +0200

    Register Security Middleware for ==> webApi project

--- ServerApp/dothelpnationServerApp/WebApi/App_Start/Startup.cs
+++ ServerApp/dothelpnationServerApp/WebApi/App_Start/Startup.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Owin.Security.Infrastructure;
+using Owin;
+using System.Web.Http;
+using WebApi.Authorization;
 
+[ assembly: OwinStartup(typeof(WebApi.App_Start.Startup))]
 namespace WebApi.App_Start
 {
     public class Startup
     {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            var myProvider = new AuthorizationServerProvider();
+
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(5),
+                Provider = myProvider
+            };
+
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
     }
 }
---
+++ ServerApp/dothelpnationServerApp/WebApi/Authorization/AuthorizationServerProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Claims;
+using Microsoft.Owin.Security;
+using System.Security.Principal;
+using Microsoft.Owin.Security.OAuth;
+
+namespace WebApi.Authorization
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+    }
+}
