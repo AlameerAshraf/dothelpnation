a0a2355718a5ca7a390288dc600144d8bafa61de
Alameer Ashraf <alamiir.ashraf@gmail.com>
2018-08-29 23:19:58 +0200

    Facebook login updates 🎉

--- MobileApp/src/pages/dhn-login/dhn-login.ts
+++ MobileApp/src/pages/dhn-login/dhn-login.ts
@@ -6,28 +6,29 @@ import { ValidationSupplier } from './../../CoreAssestiveModules/forms/Validatio
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Url } from '../../CoreAssestiveModules/Url';
 import { DataService } from '../../CoreAssestiveModules/Services/DataService';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 @IonicPage()
 @Component({
-  selector: 'page-dhn-login',
-  templateUrl: 'dhn-login.html',
+  selector: "page-dhn-login",
+  templateUrl: "dhn-login.html"
 })
 export class DhnLoginPage {
-  dir: string = 'ltr';
+  dir: string = "ltr";
   revDir: string;
   data = {
-    forgotPassword : "" ,
-    subtitle : "" ,
-    title : "" ,
-    labelEmail: "" ,
-    email : "" ,
-    labelPassword : "",
-    password : "",
-    login : "" ,
-    facebook : "" ,
+    forgotPassword: "",
+    subtitle: "",
+    title: "",
+    labelEmail: "",
+    email: "",
+    labelPassword: "",
+    password: "",
+    login: "",
+    facebook: "",
     GooglePlus: "",
-    register : "",
-    logo : 'assets/img/2.png'
+    register: "",
+    logo: "assets/img/2.png"
   };
 
   email: string;
@@ -36,20 +37,33 @@ export class DhnLoginPage {
   validations = {};
   loginform: FormGroup;
 
-  constructor(public navCtrl: NavController,
-    public navParams: NavParams ,
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
     private storage: Storage,
     private DataService: DataService,
-    private translate: TranslateService ,
-    private formBuilder: FormBuilder) {
-      this.translate.get([
-        'FORGETPASSWORD_LABEL' , 'LOGIN_PAGE_SUBTITLE' , 'LOGIN_PAGE_TITLE' , 'EMAIL_LABEL' ,
-        'EMAIL_PLACEHOLDER' , 'PASSWORD_LABEL' , 'PASSWORD_PLACEHOLDER' , 'LOGIN_PAGE_LABEL' ,
-        'FACEBOOK_REGISTER_BUTTON' , 'GOOGLE_PLUS_REGISTER_BUTTON' , 'REGISTER_PAGE_LABEL'
-      ]).subscribe((values) =>{
-        if(this.dir == "rtl"){
-          this.revDir = "ltr"
-        } else if (this.dir == "ltr"){
+    private translate: TranslateService,
+    private fb: Facebook,
+    private formBuilder: FormBuilder
+  ) {
+    this.translate
+      .get([
+        "FORGETPASSWORD_LABEL",
+        "LOGIN_PAGE_SUBTITLE",
+        "LOGIN_PAGE_TITLE",
+        "EMAIL_LABEL",
+        "EMAIL_PLACEHOLDER",
+        "PASSWORD_LABEL",
+        "PASSWORD_PLACEHOLDER",
+        "LOGIN_PAGE_LABEL",
+        "FACEBOOK_REGISTER_BUTTON",
+        "GOOGLE_PLUS_REGISTER_BUTTON",
+        "REGISTER_PAGE_LABEL"
+      ])
+      .subscribe(values => {
+        if (this.dir == "rtl") {
+          this.revDir = "ltr";
+        } else if (this.dir == "ltr") {
           this.revDir = "rtl";
         }
         this.data.forgotPassword = values.FORGETPASSWORD_LABEL;
@@ -68,59 +82,110 @@ export class DhnLoginPage {
     let result_translation = new ValidationSupplier(translate);
     this.validations = result_translation.getValidationMessages();
 
-    this.loginform =  this.formBuilder.group({
-      email :[''] ,
-      password : ['']
+    this.loginform = this.formBuilder.group({
+      email: [""],
+      password: [""]
     });
-
-
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DhnLoginPage');
+    console.log("ionViewDidLoad DhnLoginPage");
   }
 
-  onForgetPassword(event){
+  onForgetPassword(event) {
     console.log(event);
   }
 
   onLogin(event) {
     this.DataService.Post(`${Url.SecurityLocalTunnul()}/token`, null, null, {
-      "grant_type": "password",
-      "client_id": "dothelpnation",
-      "client_secret": "**dothelpmobile",
-      "password": this.password,
-      "username": this.email
-    }).subscribe((accessToken) => {
-      this.DataService.Get(`${Url.ApiUrlLocalTunnul()}/GetLoginedUser` , {Email : this.email})
-      .subscribe((UserData) => {
+      grant_type: "password",
+      client_id: "dothelpnation",
+      client_secret: "**dothelpmobile",
+      password: this.password,
+      username: this.email
+    }).subscribe(accessToken => {
+      this.DataService.Get(`${Url.ApiUrlLocalTunnul()}/GetLoginedUser`, {
+        Email: this.email
+      }).subscribe(UserData => {
         console.log(UserData);
         var access_token_auth = {
-          "access_token": accessToken.access_token,
-          "expires_in": accessToken.expires_in,
-          "token_type": accessToken.token_type
+          access_token: accessToken.access_token,
+          expires_in: accessToken.expires_in,
+          token_type: accessToken.token_type
         };
-        this.storage.set('access_token', access_token_auth);
-        this.navCtrl.setRoot('DhnHomeTabsPage' , UserData);
+        this.storage.set("access_token", access_token_auth);
+        this.navCtrl.setRoot("DhnHomeTabsPage", UserData);
+      });
+    });
+  }
+
+  onFaceBookLogin(event) {
+    this.fb
+      .login(["public_profile", "email"])
+      .then((res: FacebookLoginResponse) => {
+        if (res.status == "connected") {
+          this.getUserDestailsFBGraphCall(res.authResponse.userID);
+        }
       })
-
-    })
+      .catch(e => console.log(e));
   }
 
-  onFaceBookLogin(event){
+  onGoogleLogin(event) {
     console.log(event);
   }
 
-  onGoogleLogin(event){
+  onRedierctToRegister(event) {
+    this.navCtrl.push("DhnRegisterPage");
+  }
+
+  onEvent(event) {
     console.log(event);
   }
 
-  onRedierctToRegister(event){
-    this.navCtrl.push('DhnRegisterPage');
+  // Private Methods
+  getUserDestailsFBGraphCall(userId) {
+    this.fb
+      .api("/" + userId + "/?fields=id,email,name,picture,birthday", [
+        "public_profile",
+        "user_birthday",
+        "email"
+      ])
+      .then(res => {
+        // console.log("https://graph.facebook.com/" + userId + "/picture?type=large")
+        let newUserData = {
+          name : res.name ,
+          mobile : "" ,
+          age : "",
+          email : res.email ,
+          password : "***",
+          photo: "https://graph.facebook.com/" + userId + "/picture?type=large" , 
+          created_at : new Date(),
+          updated_at : null
+        }
+        this.DataService.Post(`${Url.ApiUrlLocalTunnul()}/CraeteUser` , newUserData).subscribe((x) => {
+          if(x){
+            this.DataService.Post(`${Url.SecurityLocalTunnul()}/token`,null , null , {
+              "grant_type" : "password",
+              "client_id" : "dothelpnation",
+              "client_secret" : "**dothelpmobile",
+              "password" : "***" ,
+              "username" : res.email
+            }).subscribe((accessToken)=>{
+              var access_token_auth = {
+                "access_token" : accessToken.access_token ,
+                "expires_in" : accessToken.expires_in,
+                "token_type" : accessToken.token_type
+              };
+    
+              this.storage.set('access_token' , access_token_auth);
+              this.navCtrl.setRoot('DhnHomeTabsPage' , newUserData);
+            })
+          }
+        });
+        console.log(res);
+      })
+      .catch(e => {
+        console.log(e);
+      });
   }
-
-  onEvent(event){
-    console.log(event);
-  }
-
 }
