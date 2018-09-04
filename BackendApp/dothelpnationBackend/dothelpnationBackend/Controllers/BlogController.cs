24b9fa7d5c427af0174261e461bfec1d50dde509
Alameer Ashraf <alamiir.ashraf@gmail.com>
2018-09-05 01:53:20 +0200

    Get ride of 'ngrok' and add publish profile in local iis 🌹

--- BackendApp/dothelpnationBackend/dothelpnationBackend/Controllers/BlogController.cs
+++ BackendApp/dothelpnationBackend/dothelpnationBackend/Controllers/BlogController.cs
@@ -114,8 +114,8 @@ namespace dothelpnationBackend.Controllers
                     );
 
                     file.SaveAs(path);
-                    photoPath = HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority) + "/BlogPhotos/" + fileName;
-                    //photoPath = "http://f6cd5256.ngrok.io" + "/BlogPhotos/" + fileName;
+                    //photoPath = HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority) + "/BlogPhotos/" + fileName;
+                    photoPath = "http://5d62cc17.ngrok.io" + "/BlogPhotos/" + fileName;
                     ImageUploaded = true;
                 }
             }
