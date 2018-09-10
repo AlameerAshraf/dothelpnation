d3fd937c4632084a98b2941c6e4a977267df65fc
Alameer Ashraf <alamiir.ashraf@gmail.com>
2018-09-10 04:38:19 +0200

    deployment updates 🎈

--- MobileApp/src/CoreAssestiveModules/Url.ts
+++ MobileApp/src/CoreAssestiveModules/Url.ts
@@ -1,8 +1,8 @@
 
 
 export class Url {
-  public static ApiUrlLocalTunnul(): string { return "http://b86d33e0.ngrok.io/api" }
+  public static ApiUrlLocalTunnul(): string { return "http://www.saydala.online/api" }
   public static ApiUrl(): string { return "http://localhost:51894/api" }
   public static Security(): string { return "http://localhost:51894" }
-  public static SecurityLocalTunnul(): string { return "http://b86d33e0.ngrok.io" }
+  public static SecurityLocalTunnul(): string { return "http://www.saydala.online" }
 }
