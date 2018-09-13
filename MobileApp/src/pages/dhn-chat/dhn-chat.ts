5ad723bd002fa1e07377aa23c41765cea0112972
Alameer Ashraf <alamiir.ashraf@gmail.com>
2018-09-13 02:08:00 +0200

    chat template created and integrated 🎉✅

--- MobileApp/src/pages/dhn-chat/dhn-chat.ts
+++ MobileApp/src/pages/dhn-chat/dhn-chat.ts
@@ -9,13 +9,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'dhn-chat.html',
 })
 export class DhnChatPage {
-  chatData;
+  currentUserName;
+  currentUserId;
+  chats : any[];
+
+
+  // Current Sent message from user 
+  newMessage: string;
+
+
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams) {
       let chatParamters = navParams.get("MessagingParams");
-      this.chatData = chatParamters;
+      this.currentUserId = chatParamters.Id;
+      this.currentUserName = chatParamters.UserName;
+
+      // chat list .. 
+      this.chats = [
+        { userId : '22' , sendDate : new Date() , message : '3am elnas !' , showMessage : "false" , userName : "Ahmed Elmasry"},
+        { userId : '33' , sendDate : new Date() , message : 'shaba7 elsenin !' , showMessage : "false" , userName : "AlameerAshraf" },
+      ]
+  }
+
+
+  sendMessage(){
+
+    this.chats.push({
+      userId : '33' ,
+      sendDate : new Date() ,
+      message : this.newMessage,
+      userName : "AlameerAshraf" ,
+      showMessage : "false"
+    });
+    this.newMessage = "";
   }
 
   ionViewDidLoad() {
