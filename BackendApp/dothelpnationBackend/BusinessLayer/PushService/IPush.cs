cbb6c966d5a7768ff87fbed5f09bfbea4babf5ed
Alameer Ashraf <alamiir.ashraf@gmail.com>
2018-10-03 04:56:29 +0200

    add push services and resolver in business layer to serve push notifications 😎

---
+++ BackendApp/dothelpnationBackend/BusinessLayer/PushService/IPush.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.PushService
+{
+    public interface IPush
+    {
+        void SendNotificationsToFCM(string serverApiKey , string senderId, string targetDeviceToken, string message);
+        void SendNotificationsToAPNS(string passPhrase, string deviceToken, string message);
+    }
+}
---
+++ BackendApp/dothelpnationBackend/BusinessLayer/PushService/Push.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace BusinessLayer.PushService
+{
+    public class Push: IPush
+    {
+        public void SendNotificationsToAPNS(string passPhrase, string deviceToken, string message)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public void SendNotificationsToFCM(string serverApiKey,string senderId, string targetDeviceToken, string message)
+        {
+            try
+            {
+                var notification = message;
+                WebRequest tRequest = WebRequest.Create("https://fcm.googleapis.com/fcm/send");
+                tRequest.Method = "post";
+                tRequest.ContentType = "application/x-www-form-urlencoded;charset=UTF-8";
+                tRequest.Headers.Add(string.Format("Authorization: key={0}" , serverApiKey));
+                tRequest.Headers.Add(string.Format("Sender: id={0}", senderId));
+
+                string postData = "data.message=" + message + "&data.time=" + System.DateTime.Now.ToString() + "&registration_id=" + targetDeviceToken + "";
+
+                Byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+                tRequest.ContentLength = byteArray.Length;
+                using (Stream dataStream = tRequest.GetRequestStream())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                    using (WebResponse tResponse = tRequest.GetResponse())
+                    {
+                        using (Stream dataStreamResponse = tResponse.GetResponseStream())
+                        {
+                            using (StreamReader tReader = new StreamReader(dataStreamResponse))
+                            {
+                                String sResponseFromServer = tReader.ReadToEnd();
+                                //result.Response = sResponseFromServer;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
