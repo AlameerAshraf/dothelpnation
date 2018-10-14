881323eabe4644c4f8db425752124ef8d221b87d
Alameer Ashraf <alamiir.ashraf@gmail.com>
2018-10-15 00:48:42 +0200

    Update urls for online api call

--- MobileApp/src/CoreAssestiveModules/Url.ts
+++ MobileApp/src/CoreAssestiveModules/Url.ts
@@ -1,8 +1,8 @@
 
 
 export class Url {
-  public static ApiUrlLocalTunnul(): string { return "http://01b52a0b.ngrok.io/api" }
+  public static ApiUrlLocalTunnul(): string { return "http://www.saydala.online//api" }
   public static ApiUrl(): string { return "http://localhost:51894/api" }
   public static Security(): string { return "http://localhost:51894" }
-  public static SecurityLocalTunnul(): string { return "http://01b52a0b.ngrok.io" }
+  public static SecurityLocalTunnul(): string { return "http://www.saydala.online/" }
 }
