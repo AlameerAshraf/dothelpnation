8bf374e50c8f7b233cd79991425a7f3facdb2b73
Alameer Ashraf <alamiir.ashraf@gmail.com>
2018-11-06 20:36:15 +0200

    fix deployements issues

--- MobileApp/src/pages/tutorial/tutorial.ts
+++ MobileApp/src/pages/tutorial/tutorial.ts
@@ -42,17 +42,27 @@ export class TutorialPage {
           {
             title: values.TUTORIAL_SLIDE1_TITLE,
             description: values.TUTORIAL_SLIDE1_DESCRIPTION,
-            image: 'assets/img/bjork-live.jpg',
+            image: 'assets/img/blood-donation.jpg',
           },
           {
             title: values.TUTORIAL_SLIDE2_TITLE,
             description: values.TUTORIAL_SLIDE2_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-2.png',
+            image: 'assets/img/found.jpg',
           },
           {
             title: values.TUTORIAL_SLIDE3_TITLE,
             description: values.TUTORIAL_SLIDE3_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-3.png',
+            image: 'assets/img/help-pets.jpg',
+          },
+          {
+            title: values.TUTORIAL_SLIDE3_TITLE,
+            description: values.TUTORIAL_SLIDE3_DESCRIPTION,
+            image: 'assets/img/miss.jpg',
+          },
+          {
+            title: values.TUTORIAL_SLIDE3_TITLE,
+            description: values.TUTORIAL_SLIDE3_DESCRIPTION,
+            image: 'assets/img/missing.jpg',
           }
         ];
       });
