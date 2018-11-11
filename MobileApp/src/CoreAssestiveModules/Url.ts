c09b8ea9368b8960ba7ae4b02a9a01b88619bebf
Alameer Ashraf <alamiir.ashraf@gmail.com>
2018-11-11 20:02:43 +0200

    fix deployements issues

--- MobileApp/src/CoreAssestiveModules/Url.ts
+++ MobileApp/src/CoreAssestiveModules/Url.ts
@@ -1,8 +1,8 @@
 
 
 export class Url {
-  public static ApiUrlLocalTunnul(): string { return "http://468a7ad2.ngrok.io/api" }
+  public static ApiUrlLocalTunnul(): string { return "http://a4dad056.ngrok.io/api" }
   public static ApiUrl(): string { return "http://localhost:51894/api" }
   public static Security(): string { return "http://localhost:51894" }
-  public static SecurityLocalTunnul(): string { return "http://468a7ad2.ngrok.io" }
+  public static SecurityLocalTunnul(): string { return "http://a4dad056.ngrok.io" }
 }
