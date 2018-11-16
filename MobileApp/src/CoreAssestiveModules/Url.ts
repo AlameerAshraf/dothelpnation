9d7faca18b742dc2fa0cedb6434a8f4549705f4d
Alameer Ashraf <alamiir.ashraf@gmail.com>
2018-11-17 01:33:51 +0200

    fix deployements issues

--- MobileApp/src/CoreAssestiveModules/Url.ts
+++ MobileApp/src/CoreAssestiveModules/Url.ts
@@ -5,4 +5,4 @@ export class Url {
   public static ApiUrl(): string { return "http://localhost:51894/api" }
   public static Security(): string { return "http://localhost:51894" }
   public static SecurityLocalTunnul(): string { return "http://www.saydala.online" }
-}
+} 
